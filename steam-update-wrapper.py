1b3b21f02afec95d8c151a0a240246feb5b3d395
Oli Wright <github@falo.co.uk>
2020-10-26 21:31:50 +0000

    Game Update and Launch

--- steam-update-wrapper.py
+++ steam-update-wrapper.py
@@ -85,16 +85,30 @@ def parsebranch(extraflags):
     return 'public'
 
 def updategame(steamcmd, installdir, appid, extraflags):
+    try:
+        fullsteamcmd = f'{steamcmd} +login anonymous +force_install_dir {installdir} +app_update {appid} {extraflags} +quit'
+        logger.info(f'Calling: {fullsteamcmd}')
+        process = subprocess.run(fullsteamcmd)
+        if process.returncode == 0:
+            logger.info(f'Steamcmd exited successfully')
+        else:
+            logger.error(f'Steamcmd failed in some way')
+            raise Exception("Steamcmd returned a non-zero exit code")
+    except:
+        logger.error(f'There was an issue running the steamcmd update.  Check your configuration.')
+        raise
 
-    print('updating game')
+def startgameprocess(commandline):
+    try:
+        process = subprocess.Popen(commandline)
+        logger.info(f'Successfully called game executable')
+        return process
+    except:
+        logger.error(f'There was an issue launching the background process')
+        raise
 
-def startgameprocess():
-    print('starting game')
-
-def stopgameprocess():
+def stopgameprocess(process):
     print('stopping game')
-
-
 
 def main():
     initlogs()
@@ -116,6 +130,14 @@ def main():
         branch = parsebranch(steamcmdextras)
         logger.info(f'Branch set to {branch}')
 
+    steamcmd = configdata.get('steamcmd', 'steamcmd')
+    logger.info(f'Steamcmd will run as {steamcmd}')
+
+    installdir = configdata.get('installdir', None)
+    if installdir is None:
+        logger.info(f'No installdir set in {configfilename}.  Defaulting to current directory.')
+        installdir = '.'
+
     checkinterval = configdata.get('checkinterval', 10)
 
     global gamecommand
@@ -126,13 +148,20 @@ def main():
     if len(sys.argv) > 1:
          gamecommand = sys.argv[1:]
 
-    logger.info(f'Game will be executed as: {" ".join(gamecommand)}')
-
     # Store last checked time
     lastupdatecheck = time.time()
     logger.info(f'Saved last checked time as {time.ctime(lastupdatecheck)}.  Attempting initial game update.')
 
-    # Update game
+    # Update the game for first run
+    #updategame(steamcmd, installdir, appid, steamcmdextras)
+
+    logger.info(f'Game updated.  Starting game using {" ".join(gamecommand)}.')
+    gameprocess = startgameprocess(gamecommand)
+
+    logger.info(f'Sleeping for 10s')
+    time.sleep(10)
+    stopgameprocess(gameprocess)
+
     # Launch game
     #logger.info(f"Scheduling update process to run every {checkinterval} minutes")
     #time.sleep(checkinterval * 60)
@@ -145,9 +174,6 @@ def main():
     #   Re-Launch game
     # Sleep for checkinterval
 
-
-
-    #
 
 
 if __name__ == "__main__":
